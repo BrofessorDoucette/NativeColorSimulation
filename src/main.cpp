e2b174fa6e592672125c766b87928292d3610d1c
brofessordoucette <brofessordoucette@gmail.com>
2023-11-11 20:30:36 -0600

    Set up OpenGL Context

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,33 @@
 #include <iostream>
+#include <glad/glad.h>
+#define GLFW_INCLUDE_NONE
 #include <GLFW/glfw3.h>
+
+
+void error_callback(int error, const char* description)
+{
+    fprintf(stderr, "Error: %s\n", description);
+}
+
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
+}
 
 int main(){
 
-    GLFWwindow* window;
+
     if(!glfwInit()){
 
         std::cout << "Failed to init glfw!" << std::endl;
         exit(EXIT_FAILURE);
     }
 
-    window = glfwCreateWindow(300, 300, "NativeColorSimulation", NULL, NULL);
+    glfwSetErrorCallback(error_callback);
+
+    GLFWwindow* window;
+    window = glfwCreateWindow(1920, 1080, "NativeColorSimulation", NULL, NULL);
     if(!window){
 
 
@@ -19,11 +36,18 @@ int main(){
 
     }
 
+    glfwMakeContextCurrent(window);
+    gladLoadGL();
+    glfwSetKeyCallback(window, key_callback);
+    glfwSwapInterval(1);
+
     while(!glfwWindowShouldClose(window)){
+
+        glClearColor(255, 0, 0, 1.0);
+        glClear(GL_COLOR_BUFFER_BIT);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
-
     }
 
     glfwTerminate();
