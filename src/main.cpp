e23c14cb820f5dfafc312bea581793071a7190f1
brofessordoucette <brofessordoucette@gmail.com>
2023-11-11 22:38:10 -0600

    Set up a basic vertex and fragment shader

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
-#include <glad/glad.h>
 #define GLFW_INCLUDE_NONE
 #include <GLFW/glfw3.h>
+#include <glad/glad.h>
 
 
 void error_callback(int error, const char* description)
@@ -19,6 +19,49 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
     glViewport(0, 0, width, height);
 }  
+
+static unsigned int CompileShader(unsigned int type, const std::string& source)
+{
+    unsigned int id = glCreateShader(type);   
+    const char* src = source.c_str();
+    glShaderSource(id, 1, &src, nullptr);
+    glCompileShader(id);
+
+    int result;
+    glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+
+    if(result == GL_FALSE)
+    {
+        int length;
+        glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+        char* message = (char*) alloca(length*sizeof(char));
+        glGetShaderInfoLog(id, length, &length, message);
+        std::cout << "Failed to compile " << (type == GL_VERTEX_SHADER ? "vertex" : "fragment") << "shader." << std::endl;
+        std::cout << message << std::endl;
+
+
+    }
+
+    return id;
+}
+
+static unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader)
+{
+
+    unsigned int program = glCreateProgram();
+    unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
+    unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
+
+    glAttachShader(program, vs);
+    glAttachShader(program, fs);
+    glLinkProgram(program);
+    glValidateProgram(program);
+
+    glDeleteShader(vs);
+    glDeleteShader(fs);
+
+    return program;
+}
 
 int main(){
 
@@ -49,9 +92,11 @@ int main(){
 
     gladLoadGL();
 
+    std::cout << "Loaded OpenGL Version: " << glGetString(GL_VERSION) << std::endl;
+    
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);  
 
-    float positions[6] = {
+    float positions[9] = {
         -0.5f, -0.5f,
         0.0f, 0.5f,
         0.5f, -0.5f
@@ -64,6 +109,28 @@ int main(){
 
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0);
+
+    std::string vertexShader = "#version 330 core\n"
+                                "\n"
+                                "layout(location=0) in vec4 position;"
+                                "\n"
+                                "void main()\n"
+                                "{\n"
+                                "gl_Position = position;\n"
+                                "}\n";
+
+    std::string fragmentShader = "#version 330 core\n"
+                                "\n"
+                                "layout(location=0) out vec4 color;"
+                                "\n"
+                                "void main()\n"
+                                "{\n"
+                                "color = vec4(1.0, 0.0, 1.0, 1.0);\n"
+                                "}\n";
+
+
+    unsigned int shader = CreateShader(vertexShader, fragmentShader);
+    glUseProgram(shader);
 
     while(!glfwWindowShouldClose(window)){
 
